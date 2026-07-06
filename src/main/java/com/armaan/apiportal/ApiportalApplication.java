f3522e3f94a098978f3a39a215cd1c247b3c816f
Armaan Mulani <armaanmulani941@gmail.com>
2026-07-06 20:04:17 +0530

    Enable scheduling in ApiportalApplication

--- src/main/java/com/armaan/apiportal/ApiportalApplication.java
+++ src/main/java/com/armaan/apiportal/ApiportalApplication.java
@@ -2,8 +2,10 @@ package com.armaan.apiportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiportalApplication {
 
 	public static void main(String[] args) {
