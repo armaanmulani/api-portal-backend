3a8e5e2df036d8221f1e910ffb52fd1f85918e3f
Armaan Mulani <armaanmulani941@gmail.com>
2026-07-06 20:13:28 +0530

    Update SecurityConfig.java

--- src/main/java/com/armaan/apiportal/config/SecurityConfig.java
+++ src/main/java/com/armaan/apiportal/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
